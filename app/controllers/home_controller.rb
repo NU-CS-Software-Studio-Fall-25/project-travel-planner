b9f654e838c929fea2841992c061846bcd1eddc8
Sirui <ray_chensirui@hotmail.com>
2025-10-07 15:06:47 -0500

    route fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    # Redirect logged-in users to their profile page
+    if logged_in?
+      redirect_to current_user
+    end
+    # Non-logged-in users will see the Rails home page (not the static index.html)
   end
 end
