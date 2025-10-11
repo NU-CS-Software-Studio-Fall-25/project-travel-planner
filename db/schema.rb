83af37319c5898add69b7fc465c245d06f8e2ea0
Ailin Chu <eileenchuu@gmail.com>
2025-10-10 20:34:48 -0500

    update

--- db/schema.rb
+++ db/schema.rb
@@ -57,6 +57,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_09_200013) do
     t.string "general_purpose"
     t.string "trip_scope"
     t.string "trip_type"
+    t.text "itinerary"
+    t.text "details"
+    t.text "description"
     t.index ["destination_id"], name: "index_travel_plans_on_destination_id"
     t.index ["user_id"], name: "index_travel_plans_on_user_id"
   end
@@ -64,9 +67,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_09_200013) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "recommendations", "destinations"
