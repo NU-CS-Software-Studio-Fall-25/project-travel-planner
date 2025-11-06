bef0ffa73d99a2c0e418c70a3c6969667261bf5f
Ailin Chu <eileenchuu@gmail.com>
2025-11-05 22:38:31 -0600

    Fix PostgreSQL migration for budget_breakdown column

--- db/migrate/20251102210346_change_budget_breakdown_to_json_in_travel_plans.rb
+++ db/migrate/20251102210346_change_budget_breakdown_to_json_in_travel_plans.rb
@@ -1,5 +1,22 @@
 class ChangeBudgetBreakdownToJsonInTravelPlans < ActiveRecord::Migration[8.0]
-  def change
-    change_column :travel_plans, :budget_breakdown, :json
+  def up
+    # For PostgreSQL, we need to specify how to cast the existing data
+    if ActiveRecord::Base.connection.adapter_name == 'PostgreSQL'
+      execute <<-SQL
+        ALTER TABLE travel_plans 
+        ALTER COLUMN budget_breakdown TYPE json 
+        USING CASE 
+          WHEN budget_breakdown IS NULL THEN NULL
+          WHEN budget_breakdown::text = '' THEN NULL
+          ELSE budget_breakdown::json 
+        END;
+      SQL
+    else
+      change_column :travel_plans, :budget_breakdown, :json
+    end
+  end
+
+  def down
+    change_column :travel_plans, :budget_breakdown, :text
   end
 end
